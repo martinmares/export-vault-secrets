e95da67b22a2fe60b37d8d8dd0df44ec2d7a5e1d
Martin Mareš <mares@datalite.cz>
2024-06-06 08:56:00 +0200

    Refactor to clap and TOML config

---
+++ src/conf.rs
@@ -0,0 +1,61 @@
+#![allow(dead_code)]
+
+use twelf::config;
+use twelf::reexports::serde::{Deserialize, Serialize};
+
+#[config]
+#[derive(Debug)]
+pub struct Config {
+    vault: Vault,
+    vars: Vec<Var>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Vault {
+    server_url: String,
+    auth_role: String,
+    auth_login_mount: String,
+    kv_mount: String,
+    id_token: String,
+    ssl_certs: String,
+    path: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Var {
+    key: String,
+    export_to: String,
+}
+
+impl Config {
+    pub fn get_vars(&self) -> &Vec<Var> {
+        &self.vars
+    }
+    pub fn get_vault(&self) -> &Vault {
+        &self.vault
+    }
+}
+
+impl Vault {
+    pub fn get_server_url(&self) -> &str {
+        &self.server_url
+    }
+    pub fn get_auth_role(&self) -> &str {
+        &self.auth_role
+    }
+    pub fn get_auth_login_mount(&self) -> &str {
+        &self.auth_login_mount
+    }
+    pub fn get_kv_mount(&self) -> &str {
+        &self.kv_mount
+    }
+    pub fn get_id_token(&self) -> &str {
+        &self.id_token
+    }
+    pub fn get_ssl_certs(&self) -> &str {
+        &self.ssl_certs
+    }
+    pub fn get_path(&self) -> &str {
+        &self.path
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,83 @@
+mod conf;
+
+use clap::{command, value_parser, Arg};
+use env_logger::Env;
+use log::{error, info};
 use std::env;
-use tracing::*;
+use std::path::PathBuf;
+use twelf::Layer;
 use vaultrs::auth::oidc;
 use vaultrs::client::{Client, VaultClient, VaultClientSettingsBuilder};
 use vaultrs::kv1;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let matches = command!()
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("Sets a config file")
+                .value_parser(value_parser!(PathBuf))
+                .required(true),
+        )
+        .get_matches();
+
+    info!("Application started!");
+
+    let config_path = if let Some(value) = matches.get_one::<PathBuf>("config") {
+        value.to_owned()
+    } else {
+        panic!("config path must be set!")
+    };
+
+    info!("Args config_path={:?}", config_path);
+
+    let config: conf::Config;
+
+    match conf::Config::with_layers(&[Layer::Toml(config_path.clone())]) {
+        Ok(value) => {
+            config = value;
+        }
+        Err(e) => {
+            error!("Error loading config {:?}, error: {:?}", config_path, e);
+            panic!("Failed to load config file with name {:?}!", config_path)
+        }
+    }
 
     // let vault_server = env::var("VAULT_SERVER_URL").unwrap();
-    let vault_server = "https://devops.cetin:8200";
-    let vault_path = String::from("data/gitlab/it/tsm_group/tsm-apps/tsm-build-app/test");
-    let vault_role = Some(env::var("VAULT_AUTH_ROLE").unwrap());
-    // let vault_role = Some("devops_tools_production_vault_tsm2_ro".to_string());
-    let vault_id_token = env::var("VAULT_ID_TOKEN").unwrap();
-    let ssl_certs = env::var("SSL_CERT_FILE").unwrap();
-    // let vault_id_token = "eyJraWQiOiJJWDFYUTNJLXdpenlpZ1BiWDlTa25lQjROSUUxem5HNGMzRUhSbmJ1MTkwIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJuYW1lc3BhY2VfaWQiOiIxMjMiLCJuYW1lc3BhY2VfcGF0aCI6Iml0L3RzbV9ncm91cC90c20tYXBwcyIsInByb2plY3RfaWQiOiIxMjAiLCJwcm9qZWN0X3BhdGgiOiJpdC90c21fZ3JvdXAvdHNtLWFwcHMvdHNtLWJ1aWxkLWFwcCIsInVzZXJfaWQiOiI0OSIsInVzZXJfbG9naW4iOiJ4MDU0OTk4MyIsInVzZXJfZW1haWwiOiJtYXJ0aW4ubWFyZXNAY2V0aW4uY3oiLCJwaXBlbGluZV9pZCI6IjE3MjQyNiIsInBpcGVsaW5lX3NvdXJjZSI6InB1c2giLCJqb2JfaWQiOiI1NTAzMzciLCJyZWYiOiJ2YXVsdF9jbGllbnQiLCJyZWZfdHlwZSI6ImJyYW5jaCIsInJlZl9wYXRoIjoicmVmcy9oZWFkcy92YXVsdF9jbGllbnQiLCJyZWZfcHJvdGVjdGVkIjoiZmFsc2UiLCJydW5uZXJfaWQiOjIsInJ1bm5lcl9lbnZpcm9ubWVudCI6InNlbGYtaG9zdGVkIiwic2hhIjoiOTg2MzNhYTNiN2IzNjNlZjU0M2RiNmFhMmQ5MmI5N2I5N2M3YzVkYSIsInByb2plY3RfdmlzaWJpbGl0eSI6InByaXZhdGUiLCJjaV9jb25maWdfcmVmX3VyaSI6ImRldm9wcy5jZXRpbi9pdC90c21fZ3JvdXAvdHNtLWFwcHMvdHNtLWJ1aWxkLWFwcC8vLmdpdGxhYi1jaS55bWxAcmVmcy9oZWFkcy92YXVsdF9jbGllbnQiLCJjaV9jb25maWdfc2hhIjoiOTg2MzNhYTNiN2IzNjNlZjU0M2RiNmFhMmQ5MmI5N2I5N2M3YzVkYSIsImp0aSI6Ijc5NzYzMTVlLTU4ZTYtNGMxYS04YjJkLWY1ZTc4Y2EzNTA1MCIsImlzcyI6Imh0dHBzOi8vZGV2b3BzLmNldGluIiwiaWF0IjoxNzE3NjIxMzI4LCJuYmYiOjE3MTc2MjEzMjMsImV4cCI6MTcxNzYyNDkyOCwic3ViIjoicHJvamVjdF9wYXRoOml0L3RzbV9ncm91cC90c20tYXBwcy90c20tYnVpbGQtYXBwOnJlZl90eXBlOmJyYW5jaDpyZWY6dmF1bHRfY2xpZW50IiwiYXVkIjoiaHR0cHM6Ly9kZXZvcHMuY2V0aW46ODIwMCJ9.RT-xTlxrDulXl1pnZZkLEvgxVEhw3GW5j74qDOMARnLDqDJtzSe_-3gqqNHgAWnC_4wdCNlbiAdi0L02W-gaNsdUdxSlHrtSFd1qmpAQJc0SzU_dYJxW9rrjZEbxJnOUmlJvMaT0tSTv2rV9HuyvF_Hord2LYcofS1e4pjZLykS4eHj4uR1x31aRx_L1y8TcF0B0UXUqzmqbwhQfMYfsfvUyKZ6i7hjLBB9MS2OfPUK6OHL__Hqwj1uAg7r57Y5dk9cx6XmHSnvRQ8Bia-PcoTK-3s2647LI7rviW3SHVEfwyIxFyUajYcOuED4k9-ZsJTugu1wbFrYP6EDpVKVUCw";
+    // let vault_server = "https://devops.cetin:8200";
+    // let vault_path = String::from("data/gitlab/it/tsm_group/tsm-apps/tsm-build-app/test");
+    // let vault_role = Some(env::var("VAULT_AUTH_ROLE").unwrap());
+    // // let vault_role = Some("devops_tools_production_vault_tsm2_ro".to_string());
+    // let vault_id_token = env::var("VAULT_ID_TOKEN").unwrap();
+    // let ssl_certs = env::var("SSL_CERT_FILE").unwrap();
+    // // let vault_id_token = "eyJraWQiOiJJWDFYUTNJLXdpenlpZ1BiWDlTa25lQjROSUUxem5HNGMzRUhSbmJ1MTkwIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJuYW1lc3BhY2VfaWQiOiIxMjMiLCJuYW1lc3BhY2VfcGF0aCI6Iml0L3RzbV9ncm91cC90c20tYXBwcyIsInByb2plY3RfaWQiOiIxMjAiLCJwcm9qZWN0X3BhdGgiOiJpdC90c21fZ3JvdXAvdHNtLWFwcHMvdHNtLWJ1aWxkLWFwcCIsInVzZXJfaWQiOiI0OSIsInVzZXJfbG9naW4iOiJ4MDU0OTk4MyIsInVzZXJfZW1haWwiOiJtYXJ0aW4ubWFyZXNAY2V0aW4uY3oiLCJwaXBlbGluZV9pZCI6IjE3MjQyNiIsInBpcGVsaW5lX3NvdXJjZSI6InB1c2giLCJqb2JfaWQiOiI1NTAzMzciLCJyZWYiOiJ2YXVsdF9jbGllbnQiLCJyZWZfdHlwZSI6ImJyYW5jaCIsInJlZl9wYXRoIjoicmVmcy9oZWFkcy92YXVsdF9jbGllbnQiLCJyZWZfcHJvdGVjdGVkIjoiZmFsc2UiLCJydW5uZXJfaWQiOjIsInJ1bm5lcl9lbnZpcm9ubWVudCI6InNlbGYtaG9zdGVkIiwic2hhIjoiOTg2MzNhYTNiN2IzNjNlZjU0M2RiNmFhMmQ5MmI5N2I5N2M3YzVkYSIsInByb2plY3RfdmlzaWJpbGl0eSI6InByaXZhdGUiLCJjaV9jb25maWdfcmVmX3VyaSI6ImRldm9wcy5jZXRpbi9pdC90c21fZ3JvdXAvdHNtLWFwcHMvdHNtLWJ1aWxkLWFwcC8vLmdpdGxhYi1jaS55bWxAcmVmcy9oZWFkcy92YXVsdF9jbGllbnQiLCJjaV9jb25maWdfc2hhIjoiOTg2MzNhYTNiN2IzNjNlZjU0M2RiNmFhMmQ5MmI5N2I5N2M3YzVkYSIsImp0aSI6Ijc5NzYzMTVlLTU4ZTYtNGMxYS04YjJkLWY1ZTc4Y2EzNTA1MCIsImlzcyI6Imh0dHBzOi8vZGV2b3BzLmNldGluIiwiaWF0IjoxNzE3NjIxMzI4LCJuYmYiOjE3MTc2MjEzMjMsImV4cCI6MTcxNzYyNDkyOCwic3ViIjoicHJvamVjdF9wYXRoOml0L3RzbV9ncm91cC90c20tYXBwcy90c20tYnVpbGQtYXBwOnJlZl90eXBlOmJyYW5jaDpyZWY6dmF1bHRfY2xpZW50IiwiYXVkIjoiaHR0cHM6Ly9kZXZvcHMuY2V0aW46ODIwMCJ9.RT-xTlxrDulXl1pnZZkLEvgxVEhw3GW5j74qDOMARnLDqDJtzSe_-3gqqNHgAWnC_4wdCNlbiAdi0L02W-gaNsdUdxSlHrtSFd1qmpAQJc0SzU_dYJxW9rrjZEbxJnOUmlJvMaT0tSTv2rV9HuyvF_Hord2LYcofS1e4pjZLykS4eHj4uR1x31aRx_L1y8TcF0B0UXUqzmqbwhQfMYfsfvUyKZ6i7hjLBB9MS2OfPUK6OHL__Hqwj1uAg7r57Y5dk9cx6XmHSnvRQ8Bia-PcoTK-3s2647LI7rviW3SHVEfwyIxFyUajYcOuED4k9-ZsJTugu1wbFrYP6EDpVKVUCw";
 
-    info!("vault_server: {}", vault_server);
-    info!("vault_path: {}", vault_path);
-    info!("vault_role: {}", vault_role.clone().unwrap());
-    info!("vault_id_token: {}", vault_id_token);
+    // info!("vault_server: {}", vault_server);
+    // info!("vault_path: {}", vault_path);
+    // info!("vault_role: {}", vault_role.clone().unwrap());
+    // info!("vault_id_token: {}", vault_id_token);
 
     // Create a client
+    let vault_conf = config.get_vault();
     let mut client = VaultClient::new(
         VaultClientSettingsBuilder::default()
-            .address(vault_server)
-            .ca_certs(vec![ssl_certs])
+            .address(&vault_conf.get_server_url())
+            .ca_certs(vec![vault_conf.get_ssl_certs().to_string()])
             .build()
             .unwrap(),
     )
     .unwrap();
 
-    let auth_info = oidc::login(&client, "oidc", &vault_id_token, vault_role).await;
+    let auth_info = oidc::login(
+        &client,
+        &vault_conf.get_auth_login_mount(),
+        &vault_conf.get_id_token(),
+        Some(vault_conf.get_auth_role().to_string()),
+    )
+    .await;
 
     match auth_info {
         Ok(auth_info) => {
@@ -41,18 +86,19 @@ async fn main() -> anyhow::Result<()> {
             // let secrets: Result<HashMap<String, String>, ClientError> =
             //     kv1::get(&client, "kv", &vault_path).await;
 
-            let secrets = kv1::get_raw(&client, "kv", &vault_path).await;
+            let secrets =
+                kv1::get_raw(&client, &vault_conf.get_kv_mount(), &vault_conf.get_path()).await;
 
             match secrets {
                 Ok(secrets) => {
                     let data = secrets.data.as_object();
                     if let Some(data) = data {
-                        warn!("data: {:?}", data);
-                        for key in data.keys() {
-                            info!("global key[\"{}\"] = {:?}", key, data[key]);
-                        }
+                        // warn!("data: {:?}", data);
+                        // for key in data.keys() {
+                        //     info!("global key[\"{}\"] = {:?}", key, data[key]);
+                        // }
                         let data_inside = data["data"].as_object();
-                        warn!("data_inside: {:?}", data_inside);
+                        // warn!("data_inside: {:?}", data_inside);
                         if let Some(data_inside) = data_inside {
                             for key in data_inside.keys() {
                                 info!("data key[\"{}\"] = {:?}", key, data_inside[key]);
